d728bf46ebb7e4748f8c070f98c9b2c714b2e301
adelsaratov-web <adelsaratov@gmail.com>
2026-08-16 02:31:46 +0300

    Add Diadoc frontend backend configuration

---
+++ diadoc-config.js
@@ -0,0 +1,4 @@
+// Set this to the HTTPS URL of the deployed READ ONLY Diadoc backend.
+// Example: window.PTG_DIADOC_BACKEND = 'https://ptg-diadoc-xxxxx.run.app';
+// No secrets belong in this file.
+window.PTG_DIADOC_BACKEND = '';
